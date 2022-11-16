66880ddce795b609039da5543644b6443d4444d8
Ekaterina Kolpakova <kolpakovapost@yandex.ru>
2022-11-16 22:15:50 +0300

    Ex1 resolved

---
+++ Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите трехзначное число: ");
+int Digit = Convert.ToInt32(Console.ReadLine());
+
+while (Digit < 100 || Digit > 999)
+{
+    Console.WriteLine("Это не трехзначное число. Введите трехзначное число: ");
+    Digit = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(Digit%100/10);
+
