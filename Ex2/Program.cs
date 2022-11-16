d8f08b35a04cfcc5db0db178dbaed6d86098ed9d
Ekaterina Kolpakova <kolpakovapost@yandex.ru>
2022-11-16 22:43:57 +0300

    Ex2 resolved

---
+++ Ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Введите число: ");
+int Digit = Convert.ToInt32(Console.ReadLine());
+
+if (Digit < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    string str = Digit.ToString();
+    Console.WriteLine(str[2]);
+}
